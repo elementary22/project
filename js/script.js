8009beff708691e433bef4612730898a909971c3
elementary <bogdan.shnur@gmail.com>
2020-09-13 23:09:10 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+"use strict";
+
+let number = 5;
+const leftBorderWidth = 1;
+
+number = 10;
+console.log(number);
+
+const obj = {
+    name: "Bogdan",
+    age: 27,
+    isMarried: false
+};
+
+console.log("Bogdans age is " + obj.age);
